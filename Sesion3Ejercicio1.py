f037cfa0f0bcd52db555451ead5608390d963d25
YelitzaMR <106023674+YelitzaMR@users.noreply.github.com>
2022-05-31 10:38:00 -0400

    Add files via upload

---
+++ Sesion3Ejercicio1.py
@@ -0,0 +1,27 @@
+
+## declaramos variables
+Nombre: str = 'Yelitza'
+Apellidos: str ='Morales Rondón'
+Edad: int =(38)
+email: str='yelitzamoralesr@gmail.com'
+Telefono: int =(29320392)
+Casado: bool =True
+Con_hijos: bool =False
+Mis_amigos =['Lourdes', 'Willi', 'Shely']
+Peliculas ={
+    'Comedia romántica':'Forest Gump',
+    'Hechos reales':'La lista de Schindler',
+    'Drama':'La vida es bella',
+    'Ficción':'Jocker'
+    }
+
+##imprimimos variables
+print(Nombre)
+print (Apellidos)
+print (Edad)
+print (email)
+print (Telefono)
+print (Casado)
+print (Con_hijos)
+print (Mis_amigos)
+print (Peliculas)
